bcfb2b95dd5518b51f1683efc09856c2944245d6
RafiAkem <yc66zio@gmail.com>
2025-06-02 19:50:46 +0700

    feat:Movement added

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public FixedJoystick joystick;
+    public float moveSpeed = 100f;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        float moveX = joystick.Horizontal;
+        float moveY = joystick.Vertical;
+
+        Vector3 move = new Vector3(moveX, moveY, 0f);
+        transform.position += move * moveSpeed * Time.deltaTime;
+
+        Vector3 pos = Camera.main.WorldToViewportPoint(transform.position);
+        pos.x = Mathf.Clamp(pos.x, 0.05f, 0.95f);
+        pos.y = Mathf.Clamp(pos.y, 0.05f, 0.95f);
+        transform.position = Camera.main.ViewportToWorldPoint(pos);
     }
 }
