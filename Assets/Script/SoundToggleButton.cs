1e5006672285ce1b63763e9e9545f6f1ce155507
Daffa Muhamad <Daffamuhamadra@gmail.com>
2025-06-08 16:37:31 +0700

    fixing audio icon in main menu, still have bug on audio icon in game. BUILD DONE

--- Assets/Script/SoundToggleButton.cs
+++ Assets/Script/SoundToggleButton.cs
@@ -1,34 +1,51 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class SoundToggleButton : MonoBehaviour
 {
     public Image buttonImage;
-    public Sprite soundOnSprite; 
+    public Sprite soundOnSprite;
     public Sprite soundOffSprite;
-
-    private bool isMuted = false;
 
     private void Start()
     {
-        UpdateButtonImage();
+        if (SoundManager.Instance != null)
+        {
+            UpdateButtonImage();
+        }
+        else
+        {
+            Debug.LogError("SoundManager tidak ditemukan di scene!");
+        }
     }
 
     public void ToggleMute()
     {
-        isMuted = !isMuted;
-        SoundManager.Instance.SetMute(isMuted);
-        UpdateButtonImage();
-        Debug.Log("Sound muted: " + isMuted);
+        if (SoundManager.Instance != null)
+        {
+            bool newMuteState = !SoundManager.Instance.IsMuted();
+            SoundManager.Instance.SetMute(newMuteState);
+            UpdateButtonImage();
+            Debug.Log("Sound muted: " + newMuteState);
+        }
+        else
+        {
+            Debug.LogError("SoundManager.Instance masih null saat ToggleMute dipanggil.");
+        }
     }
 
     private void UpdateButtonImage()
     {
-        if (buttonImage != null)
+        if (buttonImage != null && SoundManager.Instance != null)
         {
+            bool isMuted = SoundManager.Instance.IsMuted();
             buttonImage.sprite = isMuted ? soundOffSprite : soundOnSprite;
         }
+    }
+
+    // Opsional: jika ingin update gambar setiap kali toggle muncul lagi
+    private void OnEnable()
+    {
+        UpdateButtonImage();
     }
 }
